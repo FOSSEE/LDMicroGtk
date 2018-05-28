bc3108e03a60505e5320bb18e67e9bed9142f41b
Rohit <rohitimandi9@zoho.com>
2018-05-28 14:12:11 +0530

    Ported the MakeMainWindowMenus function

--- ldmicro/linuxUI.cpp
+++ ldmicro/linuxUI.cpp
@@ -1,9 +1,43 @@
 #include "linuxUI/linuxUI.h"
 
+/// Menu Variables
+HWID window;
+HWID menu_box;                                              // Box for alignment
+HWID packed_menu_box;                                       // Stores the packed box
+HWID FileMenu;                                              // File Menu
+HWID EditMenu;                                              // Edit Menu
+HWID settings;                                              // Settings Menu
+HWID ProcessorMenu;                                         // Processor Menu
+HWID InstructionMenu;                                       // Instruction Menu
+HWID compile;                                               // Compile Menu
+HWID help;                                                  // Help Menu
+HWID SimulateMenu;                                          // Simulate Menu
+HWID menu_bar;                                              // Menu Bar
+HWID file_label;                                            // File menu label
+HWID edit_label;                                            // Edit menu label
+HWID instruction_label;                                     // Instruction menu label
+HWID settings_label;                                        // Settings menu label
+HWID compile_label;                                         // Compile menu label
+HWID help_label;                                            // Help menu label
+HWID simulate_label;                                        // Simulate menu label
+HWID file_menu_items;                                       // File menu item
+HWID edit_menu_items;                                       // Edit menu item
+HWID instruction_menu_items;                                // Instruction menu item
+HWID settings_menu_items;                                   // Settings menu item
+HWID processor_menu_items;                                  // Processor menu items
+HWID compile_menu_items;                                    // Compile menu item
+HWID help_menu_items;                                       // Help menu item
+HWID simulate_menu_items;                                   // Simulate menu item
+HWID file_menu_separator;                                   // File menu separator
+HWID edit_menu_separator;                                   // Edit menu separator
+HWID instruction_menu_separator;                            // Instruction menu separator
+HWID simulate_menu_separator;                               // Simulate menu separator
+
+
 /// Wraper function for gtk_window_has_toplevel_focus
 BOOL isFocus(HWND window)
 {
-    return (BOOL) gtk_window_has_toplevel_focus(GTK_WINDOW(window)));
+    return (BOOL) gtk_window_has_toplevel_focus(GTK_WINDOW(window));
 }
 
 COLORREF RGB(int red, int green, int blue)

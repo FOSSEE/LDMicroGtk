ceb88448976243a8c5da2549e82f0d874cad16c9
akshay-c <akshaychipkar@gmail.com>
2018-08-31 09:29:19 +0530

    Background Color correction

--- ldmicro/ldmicro.cpp
+++ ldmicro/ldmicro.cpp
@@ -1254,7 +1254,7 @@ int main(int argc, char** argv)
     gtk_window_resize (GTK_WINDOW(MainWindow), 800, 600);
     gtk_window_move(GTK_WINDOW(MainWindow), 10, 10);
     gtk_widget_override_background_color(GTK_WIDGET(MainWindow), 
-                            GTK_STATE_FLAG_NORMAL, ((HBRUSH)GetStockObject(DKGRAY_BRUSH)));
+                            GTK_STATE_FLAG_NORMAL, ((HBRUSH)GetStockObject(BLACK_BRUSH)));
     gtk_window_set_default_icon(LoadImage(Instance, LDMICRO_ICON,
                             IMAGE_ICON, 32, 32, 0));
     gtk_window_set_icon(GTK_WINDOW(MainWindow), LoadImage(Instance, LDMICRO_ICON,
--- ldmicro/maincontrols.cpp
+++ ldmicro/maincontrols.cpp
@@ -615,7 +615,8 @@ void MakeMainWindowControls(void)
 
     /// Grid for status bars
     HWID StatusGrid = gtk_grid_new();
-
+    gtk_widget_override_background_color(GTK_WIDGET(StatusGrid), 
+                            GTK_STATE_FLAG_NORMAL, ((HBRUSH)GetStockObject(DKGRAY_BRUSH)));
     /// Creating Status Bar 1 and attaching to grid
     StatusBar[0] = gtk_statusbar_new();
    
